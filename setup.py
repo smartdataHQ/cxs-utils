8b51857406908ea8c1c450d2eff3a107ccf48fb3
BinaryNavigator07 <umaiesajid@gmail.com>
2025-06-11 07:46:12 +0500

    Newest minor versions and adds missing packages.

--- setup.py
+++ setup.py
@@ -6,11 +6,15 @@ setup(
     description="Context Suite Utilities for Data Engineering",
     author="BinaryNavigator07",
     packages=find_packages(),
-    install_requires=[
-        "requests>=2.28.1",
-        "pydantic>=2.11.5",
-        "aiohttp>=3.9.1",
-        "aioresponses>=0.7.5",
+    install_requires = [
+    "requests>=2.32.4",
+    "pydantic>=2.11.5",
+    "aiohttp>=3.12.12",
+    "aioresponses>=0.7.8",
+    "unidecode>=1.4.0",
+    "jinja2>=3.1.6",
+    "pandas>=2.3.0",
+    "python-slugify>=8.0.4",
     ],
     python_requires=">=3.8",
 )
